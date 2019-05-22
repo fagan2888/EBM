55c6c8e6b64ed91dc1bfbc45dc85b541b9516a81
Henry Peterson <hankpete97@gmail.com>
2019-05-21 17:33:10 -0700

    non-const diff and RH feedback

--- test_run.py
+++ test_run.py
@@ -4,8 +4,8 @@ from EBM import EnergyBalanceModel
 
 model = EnergyBalanceModel(N_pts=401, dtmax_multiple=1e3, max_sim_years=10.0, tol=1e-8)
 
-model.initial_temperature(initial_condition='load_data', low=None, high=None)
-# model.initial_temperature(initial_condition='legendre', low=250, high=300)
+# model.initial_temperature(initial_condition='load_data', low=None, high=None)
+model.initial_temperature(initial_condition='legendre', low=250, high=300)
 
 model.insolation(insolation_type='perturbation', perturb_center=15, perturb_spread=4.94, perturb_intensity=18)
 # model.insolation(insolation_type='perturbation', perturb_center=60, perturb_spread=9.89, perturb_intensity=18)
